98322df6e1d83197891965626b7987f218ebb7dd
mvntahax <syedamuntahapk@gmail.com>
2025-10-31 15:48:35 +0500

    6th task completed.

--- task-6/server.js
+++ task-6/server.js
@@ -26,5 +26,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
